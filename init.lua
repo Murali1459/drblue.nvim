39d597fc6037df74c090e780c46c7f9611b7d7f6
Murali1459 <tallapallimurali987@gmail.com>
2024-11-08 14:48:49 +0530

    Rebased With Kickstart.nvim

--- init.lua
+++ init.lua
@@ -954,6 +954,7 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
